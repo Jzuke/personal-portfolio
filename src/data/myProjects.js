324060858be405412d5738adcad37d127060494b
Jake <jakezuke@gmail.com>
2020-05-11 19:24:53 -0400

    updated copy

--- src/data/myProjects.js
+++ src/data/myProjects.js
@@ -1,10 +1,10 @@
 const myProjects = [{
     title: "MTA-tracker",
     info: "tech: Javascript, Nodejs, Raspberry Pi, C++, lumber",
-    description: "Live transit tracker for NYC",
+    description: "Live transit tracker for NYC transit",
     imgCaptions: {
       header: "Adding to the greenery",
-      one:"Pile of stuff ( LED Matrices, Raspberry Pi, wires, other stuff )",
+      one:"Pile of stuff (LED Matrices, Raspberry Pi, wires, other stuff)",
       two:"Early iteration of the design"
     },
     links: {
@@ -12,12 +12,12 @@ const myProjects = [{
       site: "https://medium.com/@jakezuke/tracking-the-nyc-subway-from-my-apartment-eaf047ec8b7e",
       github: "https://github.com/Jzuke/MTA-tracker"
     },
-    tech: "Raspberry Pi, Node.js, C++, lumber",
+    tech: "Javascript, Node.js, Raspberry Pi, C++, lumber",
     summary:{
       paraOne:`This was a personal project I created both for utility and aesthetics.
-      It's a live tracker for NYC public transportation. The current iteration
+      It's a real-time tracker for NYC public transportation that lets me know the best time to leave my apartment to catch the train. The current iteration
       only takes advantage of the G train since that's the closest train for me, but it can be modified to pull in data from any train or
-      bus schedule. It's built using a Raspberry Pi with a few Node.js and C libraries to pull in the data and connect it to the LED matrices.`
+      bus schedule. It's built using a Raspberry Pi with a few Node.js and C++ libraries to pull in the data and connect it to the LED matrices.`
     }
   },
 
@@ -27,8 +27,8 @@ const myProjects = [{
     info: "tech: Javascript, webpack, localStorage, HTML, CSS\u2014migrating to React and Firebase",
     description: "A web app for creative writing",
     imgCaptions: {
-      header: "Minimalist word editor",
-      one: "Blank page, no entries",
+      header: "The minimalist text editor",
+      one: "Blank page with no entries",
       two: "Populated entry"
     },
     links: {
@@ -36,13 +36,13 @@ const myProjects = [{
       site: "https://boring-kirch-8243fd.netlify.app/",
       github: "https://github.com/Jzuke/writing-app"
     },
-    tech:"Javascript, HTML, CSS, webpack, localStorage",
+    tech:"Javascript, webpack, localStorage, HTML, CSS",
     summary: {
-      paraOne:`The purpose of this app is to be a catalyst for creative writing. The methodology I use here is adapted from Pat Pattison"s book 
+      paraOne:`The purpose of this app is to be a catalyst for creative writing. The methodology I use here is adapted from a book called
       Songwriting Without Boundaries. I originally built this for myself and other writer friends to practice a specific type of writing called object
-      writing. The app generates a random prompt based on a list of adjectives and verbs and then starts a timed writing session.`,
-      paraTwo: `I'm currently expanding it out into something more robust which will include the option to choose different types of writing exercises
-      along with just being an empty space to journal your thoughts. The current iteration uses Javascript and localStorage, but I'm transiting to a Firebase
+      writing, specifically meant to focus on your senses. The app generates a random prompt based on a list of adjectives and verbs and then starts a timed writing session.`,
+      paraTwo: `I'm currently expanding it out into something more robust which will connect the entries to a database, include different types of writing 
+      exercises and create the option for an empty space to journal thoughts. The current iteration uses vanilla Javascript and localStorage, but I'm transiting to a Firebase
       database and React. Also working on workshopping an actual name for this thing.`
     }
 },
@@ -55,10 +55,10 @@ const myProjects = [{
       header: "Header image on WSJ Snapchat profile"
     },
     summary:{
-      paraOne:`Working on Snapchat Discover helped provide a platform to push innovative storytelling around typical WSJ content. My role was primarily to collect assets,
-      create illustrative elements and edit video to create full editions. I also helped write out some of the scripts for the stories. My primary tools for creating these were tools from Adobe CC, including After Effects, Photoshop, Illustrator and Premiere.`,
-      paraTwo:`During my first year on this team, we were able to surpass 1 million subscribers, increase daily uniques by 19%, and increase returning users by 26%. We were also able to capture the eyes of an audience 
-      that typically does not read WSJ content. This in turn helped inform overall company strategy moving forward and the insights contributed to my role as a Product Designer.`
+      paraOne:`Snapchat Discover helped provide a platform to push innovative storytelling methods around typical WSJ content. My role was primarily to create short animated videos by collecting assets,
+      creating illustrative elements and editing video. I also helped write out some of the scripts for the stories. My primary tools for creating these videos were from Adobe CC including After Effects, Photoshop, Illustrator and Premiere.`,
+      paraTwo:`During my first year on this team, we were able to surpass 1 million subscribers, increase daily unique visitors by 19%, and increase returning users by 26%. We were also able to capture the eyes of an audience 
+      that typically does not engage with WSJ content. This in turn helped inform overall company strategy moving forward and the insights carried over to my role as a Product Designer for young audiences.`
     }
   },
 
@@ -70,10 +70,10 @@ const myProjects = [{
     imgCaptions: {
       header: "Updated homepage",
       one: "Updated internal page",
-      two: "Restructured nav",
+      two: "Restructured navigation",
       three: "Old homepage",
       four: "Old internal page",
-      five: "Old nav"
+      five: "Old navigation"
     },
     links: {
       siteType:"Website",
@@ -81,8 +81,10 @@ const myProjects = [{
     },
     tech:"Wordpress, HTML, CSS, Sketch, Photoshop",
     summary:{
-      paraOne:`A redesigned website for the non-profit Teachers Supporting Teachers. The goal for the project was to create a cohesive visual identity for the 
-      site, make the CTAs more clear and easily accessible, and to clean up the overall information architecture/navigation of the site.`
+      paraOne:`A redesigned website for the non-profit Teachers Supporting Teachers (TST). The goals for the project were to create a cohesive visual identity for the 
+      site, make the CTAs more clear and easily accessible, serve as a hub for events, allow for easy integration of a CRM  and to clean up the overall information architecture/navigation of the site.`,
+      paraTwo:`Since the redesign TST has increased overall member signup, had the site serve as a home base for their first-ever individual giving campaign which yielded over $18,000+, recieved a new grant,
+      mobilized a 15 person associate board who brought in 5 new sponsors for their benefit gala, and hooked up to their CRM and donation platform which streamlined operations.`
     }
   },
   
@@ -102,7 +104,7 @@ const myProjects = [{
     tech:"React, CSS, HTML",
     summary:{
       paraOne:`If you've ever really wanted to tape a banana to a wall, but didn't want to use real tape or a real banana, now is your chance. This project
-      uses React and some CSS/SVG animations.`
+      uses React and some CSS/SVG animations. And all it does is exactly as mentioned.`
     }
   },
 
@@ -122,10 +124,11 @@ const myProjects = [{
     },
     tech:"Post-it notes, Ricoh printer",
     summary:{
-      paraOne:`At my previous company in Nashville, we had a great window. One of those windows that can cause some serious envy (you know the ones I"m talking about)
-      We decided to use that real estate to display some monthly post-it art to help put a smile on the daily commuters faces. In this specific iteration I decided
-      to help with the task to create something not only pleasing to the eye, but a to connect the digital world to the physical. This post-it art was a Snapchat code
-      that you could actually scan to add us as a friend on the app. The tech I used here was nothing more than a a few (a lot) of stickies. And a Ricoh enterprise printer.`
+      paraOne:`At my previous company in Nashville, we had a great window. One of those windows that can cause some serious window envy (you know the ones I'm talking about).
+      We decided to use that real estate to display some monthly post-it art to help put a smile on the daily commuter's face. In this specific iteration I decided
+      to help with the task to create something not only pleasing to the eye, but to connect the digital world to the physical. This post-it art was a Snapchat code
+      that you could actually scan to add us (redpepper) as a friend on the app. The tech I used here was nothing more than a few (a lot) of post-it notes. And a handy enterprise 
+      Ricoh printer that we never had problems with..`
     }
   }]
   
